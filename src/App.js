11a2480cc4f9c07f1012b98c5311f07681bd939b
markortleb <markortleb@gmail.com>
2023-04-01 15:56:18 -0600

    adding more css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './styles/App.css';
 import TitleBar from "./components/TitleBar";
 import GameArea from "./components/GameArea";
+import Footer from "./components/Footer";
 import {useState} from "react";
 import loadCards from "./util/loadCards";
 
@@ -13,6 +14,7 @@ const App = () => {
         <div className="App">
             <TitleBar />
             <GameArea score={score} cards={cards}/>
+            <Footer />
         </div>
     );
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Footer.css";
+
+
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <span>Created by: Mark Ortleb</span>
+        </div>
+    );
+};
+
+
+export default Footer;

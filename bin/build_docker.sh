fb4d1aabab4bf5bb0598df18183b68ce8d6735dc
Mark Ortleb <markortleb@gmail.com>
2025-12-12 19:25:52 -0500

    adada

--- bin/build_docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ./import_env.sh
-
-docker build -t "$DOCKER_PROJECT_NAME" -f ../Dockerfile ../
--- bin/run_docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ./import_env.sh
-
-docker run -d -p 49152:80 --name "$DOCKER_PROJECT_NAME-container" "$DOCKER_PROJECT_NAME"
